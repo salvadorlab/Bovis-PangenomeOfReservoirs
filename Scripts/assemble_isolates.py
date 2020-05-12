0978c79e90f0a1633e28495a3b79980d7f362efe
Noah Legall <noahaus@uga.edu>
2020-05-12 12:59:17 -0400

    update assembly

--- Scripts/assemble_isolates.py
+++ Scripts/assemble_isolates.py
@@ -9,11 +9,11 @@ import re # we can do regular expression features with this
 import time # for time stamps
 
 #0. functions for script
-def logger(message):
-    print("[{}] {}".format(time.strftime('%a %H:%M:%S'),message))
+logger = lambda message: "[{}] {}".format(time.strftime('%a %H:%M:%S'),message)
 
 #1. create automatically the submission script for qsub
-logger("creating the assembly submission script")
+print(logger("creating the assembly submission script"))
+
 qsub_script = open("assemble_isolates.sh","w")
 qsub_script.write(
 """
@@ -49,13 +49,13 @@ r1 = sorted(glob.glob('*trimmed.R1.fastq'))
 r2 = sorted(glob.glob('*trimmed.R2.fastq'))
 # something new i'm using. lambdas are anonymous functions that don't need a formal name. basically quick and dirty function creation
 # saves LOC if the function is relatively simple. here, I'm creating a list of output directory names.
-scaffold_file = list(map(lambda x: re.sub('_1.fastq','_scaffold.fa',x), r1))
-assemble_dir = list(map(lambda x: re.sub('_1.fastq','_assembly',x), r1))
+scaffold_file = list(map(lambda trim: re.sub('.trimmed.R1.fastq','.scaffold.fa',trim), r1))
+assemble_dir = list(map(lambda trim: re.sub('.trimmed.R1.fastq','.assembly',trim), r1))
 
 #qsub -v reference=/path/to/reference.fa bash.sh
 for i in range(len(r1)):
     os.system("qsub -v \"r1={},r2={},out={},scaffold={}\" assemble_isolates.sh".format(r1[i],r2[i],assemble_dir[i],scaffold_file[i]))
-    logger("DeBrujin Graph Assembly occuring now between {} and {}. Output resides in {} directory".format(r1[i],r2[i],assemble_dir[i]))
+    print(logger("DeBrujin Graph Assembly occuring now between {} and {}. Output resides in {} directory".format(r1[i],r2[i],assemble_dir[i])))
 
 os.remove("assemble_isolates.sh")
-logger("All isolates submitted to the cluster")
+print(logger("All isolates submitted to the cluster"))
