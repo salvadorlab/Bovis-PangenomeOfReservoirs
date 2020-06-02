2e4fcf027dc0fe7c3e07d09dbae3d22134810fad
Noah Legall <noahaus@uga.edu>
2020-06-02 16:32:30 -0400

    add filter + genbank file

--- Scripts/pangenome_figures.R
+++ Scripts/pangenome_figures.R
@@ -20,25 +20,32 @@ accession_bar <- accession_bar[order(accession_bar$Host,accession_bar$no_in_data
 ggbarplot(accession_bar,"Species","no_in_data", fill = "Host", color = "Host",palette = c("steelblue","red"), xlab = "Host Species", ylab = "No. of isolates", lab.size = 10) + theme(axis.text.x = element_text(angle = 45, hjust = 1),axis.text=element_text(size = 15), axis.title = element_text(size = 18)) + scale_y_log10()
 
 ###2. Distribution of N50 for the assemblies
-assembly_stats <- read.csv("mbovis_transposed_report.csv",header = TRUE)
+setwd("/Users/noah_/Documents/Bovis-PangenomeOfReservoirs/Resources/")
+assembly_stats <- read.csv("mbovis_reservoir_genome_stat.tsv.csv",header = TRUE)
 
-N50_hist <- assembly_stats %>% filter(N50 > 46069.75) %>% ggplot(aes(x=N50)) +
+N50_hist <- assembly_stats %>% ggplot(aes(x=N50)) +
                    geom_histogram(binwidth = 5000, fill = "steelblue", color = "steelblue") +
                    theme_light()
 
-genome_length_hist <- assembly_stats %>% filter(Total.length < 4341873 ) %>% filter(Total.length > 4261165 ) %>% ggplot(aes(x=Total.length)) +
+genome_length_hist <- assembly_stats  %>% ggplot(aes(x=Total.length)) +
   geom_histogram(binwidth = 1000, fill = "steelblue", color = "steelblue") +
   theme_light()
 
-GC_hist <- assembly_stats %>% filter(GC.... > 65.31) %>% filter(GC.... < 65.79) %>% ggplot(aes(x=GC....)) +
+GC_hist <- assembly_stats %>% ggplot(aes(x=GC....)) +
   geom_histogram(binwidth = 0.005, fill = "steelblue", color = "steelblue") +
   theme_light()
 
-genome_frac_hist <- assembly_stats %>% filter(Genome.fraction.... > 97.84405) %>% ggplot(aes(x=Genome.fraction....)) + 
+genome_frac_hist <- assembly_stats  %>% ggplot(aes(x=Genome.fraction....)) + 
   geom_histogram(binwidth = 0.01, fill = "steelblue", color = "steelblue") +
   theme_light()
 
-(N50_hist | genome_length_hist)/(GC_hist | genome_frac_hist)
+patch_assemblies <- (N50_hist | genome_length_hist)/(GC_hist | genome_frac_hist)
+
+assembly_stats %<>%
+  filter(N50 > 45000) %>%
+  filter(Total.length < 5000000) %>%
+  filter(GC.... > 65.0 & GC.... < 65.99) %>%
+  filter(Genome.fraction.... > 97.0)
 
 ###3. Impact of Filtering on Data
 #goal: simply, just include the isolates whose names make it past the filtering scheme I devised.
@@ -46,8 +53,7 @@ genome_frac_hist <- assembly_stats %>% filter(Genome.fraction.... > 97.84405) %>
 #b. after that, I'll just recreate the Data Description figure.
 
 #a. 
-filtered_assembly_stats <- assembly_stats %>% filter(Genome.fraction.... > 97.84405) %>% filter(GC.... > 65.31) %>% filter(GC.... < 65.79) %>% filter(Total.length < 4341873 ) %>% filter(Total.length > 4261165 ) %>% filter(N50 > 46069.75)
-names <- gsub('.scaffold','',filtered_assembly_stats$??..Assembly)
+names <- gsub('.scaffold','',assembly_stats$Assembly)
 filtered_accession_metadata <- accession_meta[accession_meta$??..Sample %in% names,]
 
 #b.
