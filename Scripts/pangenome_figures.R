53bd921343398c663d2052296b5fd25087fd2d52
Noah Legall <noahaus@uga.edu>
2020-07-01 15:54:16 -0400

    update figures + notes + R script

--- Scripts/pangenome_figures.R
+++ Scripts/pangenome_figures.R
@@ -21,7 +21,7 @@ ggbarplot(accession_bar,"Species","no_in_data", fill = "Host", color = "Host",pa
 
 ###2. Distribution of N50 for the assemblies
 setwd("/Users/noah_/Documents/Bovis-PangenomeOfReservoirs/Resources/")
-assembly_stats <- read.csv("mbovis_reservoir_genome_stat.tsv.csv",header = TRUE)
+assembly_stats <- read.csv("mbovis_transposed_report.csv",header = TRUE)
 
 N50_hist <- assembly_stats %>% ggplot(aes(x=N50)) +
                    geom_histogram(binwidth = 5000, fill = "steelblue", color = "steelblue") +
@@ -54,7 +54,6 @@ assembly_stats %<>%
 
 #a. 
 names <- gsub('.scaffold','',assembly_stats$Assembly)
-filtered_accession_metadata <- accession_meta[accession_meta$??..Sample %in% names,]
 
 #b.
 accession_bar <- filtered_accession_metadata %>% group_by(Species, Host) %>%summarise(no_in_data = length(Species))
@@ -68,7 +67,7 @@ gene_pres_abs <- read.csv("mbovis_prab.csv", header = TRUE, stringsAsFactors = F
 accessory_genome <- gene_pres_abs[!(is.na(gene_pres_abs$Accessory.Fragment)),]
 core_genome <- gene_pres_abs[is.na(gene_pres_abs$Accessory.Fragment),]
 
-accessory_genome_non_unique <- accessory_genome[accessory_genome$No..isolates > 1 & accessory_genome$Annotation != "hypothetical protein",]
+accessory_genome_non_unique <- accessory_genome[accessory_genome$No..isolates > 1,]
 auxil <- gene_pres_abs %>% select(2:14)
 
 accessory_pa <- accessory_genome_non_unique %>% select(14:(ncol(accessory_genome_non_unique)))
@@ -81,6 +80,8 @@ pa_transpose <- t(data.matrix(accessory_pa))
 heatmap(pa_transpose, scale = "none", Rowv = NA, Colv = NA, col = c("white","blue"), main = "Accessory Genome Composition")
 ##Would be nice to merge a core genome phylogeny to this metadata 
 
+
+### 5. Hierarchical Clustering w/ Dendextend
 pa_dist_jaccard <- proxy::dist(pa_transpose,method = "Jaccard")
 pa_dist_euclid <- dist(pa_transpose)
 
@@ -99,6 +100,8 @@ euclidean_ac <- function(x) {
 best_linkage_euclidean <- map_dbl(mthds, euclidean_ac)
 best_linkage_jaccard <- map_dbl(mthds, jaccard_ac)
 
+
+
 prab_hc <- hclust(pa_dist_jaccard, method = "ward.D" )
 plot(prab_hc, cex = 0.4)
 rect.hclust(prab_hc, k = 3, border = 2:5)
@@ -107,8 +110,18 @@ prab_hc_viz <- plot(prab_hc, cex = 0.4);rect.hclust(prab_hc, k = 3, border = 2:5
 mbov_meta <- read.csv("ProjectBovisReservoir_metadata.csv",header = TRUE)
 
 prab_dend <- as.dendrogram(prab_hc) %>% color_branches(k=3) %>% hang.dendrogram(hang_height=0.1)
-prab_dend_labels <- as.data.frame(labels(prab_dend)) %>% left_join(mbov_meta,by=c("labels(prab_dend)" = "??..Sample"))
+prab_dend_labels <- as.data.frame(labels(prab_dend)) %>% left_join(mbov_meta,by=c("labels(prab_dend)" = "Sample"))
 prab_species <- prab_dend_labels$Host
+prab_country <- prab_dend_labels$Country
+countrydend <- rainbow_hcl(3)[prab_dend_labels$Country]
+hostdend <- rainbow_hcl(2)[prab_dend_labels$Host]
+bars <- cbind(countrydend,hostdend)
+prab_dend <- prab_dend %>% set("labels","")
+
+
+
+plot(prab_dend)
+colored_bars(bars,prab_dend)
 
 labels_colors(prab_dend) <- rainbow_hcl(2)[sort_levels_values(as.numeric(prab_dend_labels$Host))]
 labels(prab_dend) <- prab_dend_labels$Host
@@ -119,43 +132,84 @@ prab_dend <- as.dendrogram(prab_hc) %>% color_branches(k=3) %>% hang.dendrogram(
 
 
 mbov_meta <- read.csv("filtered_isolate_list.csv",header = TRUE)
-logsvd_model = logisticSVD(pa_transpose, k = 3)
 
-hostgg <- plot(logsvd_model, type = "scores") + 
-            geom_point(aes(colour = mbov_meta$Host)) + 
-            ggtitle("M. bovis Accessory Genome Clustering") +
-            theme_bw()
-hostgg$labels$colour <- "Reservoir Status"
+#### 6. PCoA
+#compute percent explained variance
+var_ex <- cmdscale(pa_dist_jaccard,k = 699)
+vars <- apply(var_ex, 2, var)
+pcoa_var_explained <- (vars/sum(vars)) * 100
+pcoa_var_explained <- pcoa_var_explained[1:10]
 
-countrygg <- plot(logsvd_model, type = "scores") + geom_point(aes(colour = mbov_meta$Country)) + ggtitle("M. bovis Accessory Genome Clustering")
-countrygg$labels$colour <- "Country of Origin"
+prin_comp <- paste0("PC",1:10)
+data <- data.frame(
+  PC=prin_comp,  
+  value=pcoa_var_explained
+)
 
-wildlifegg <- plot(logsvd_model, type = "scores") + geom_point(aes(colour = mbov_meta$Species)) + ggtitle("M. bovis Accessory Genome Clustering")
-wildlifegg$labels$colour <- "Species"
-
-pdf("logisticPCA_AccessoryGenome.pdf")
-hostgg
-countrygg
-wildlifegg
-dev.off()
-
-
-mbov.pca <- prcomp(pa_transpose,center = TRUE,scale. = TRUE)
-plot(mbov.pca, type = "scores") + geom_point(aes(colour = mbov_meta$Species))
-fviz_pca_ind(mbov.pca,geom = "point",habillage = mbov_meta$Species) + xlim(-10,20)
-
-mbov.pcoa <- cmdscale(pa_dist_jaccard,k=3)
+#create dataframe with the first 3 PCs
+mbov.pcoa <- cmdscale(pa_dist_jaccard,k=4)
 mbov.pcoa <- as.data.frame(mbov.pcoa)
 mbov.pcoa$Host <- mbov_meta$Host
 mbov.pcoa$Country <- mbov_meta$Country
 mbov.pcoa$Species <- mbov_meta$Species
-names(mbov.pcoa)[1:3] <- c('PC1', 'PC2','PC3')
+mbov.pcoa$Cattle <- mbov_meta$Domesticated
+names(mbov.pcoa)[1:4] <- c('PC1', 'PC2','PC3','PC4')
 
-Tr_PcoA <- ggplot(mbov.pcoa, aes(x = PC2, y = PC3, colour = Country, 
-                                 label = row.names(mbov.pcoa)))
-Tr_PcoA + 
-  geom_point() +
-  ggtitle("PCoA of M. bovis Accessory Genome Distance Matrix") +
+#Scree plot
+pcoa_scree <- data %>% ggplot(aes(x=reorder(PC, -value), y=value)) + 
+  geom_bar(stat = "identity",fill = "steelblue") +
+  geom_text(aes(label = round(value,1)), color = "black", position=position_dodge(width=0.9), vjust=-0.25) +
+  theme_bw() +
+  ggtitle("Principal Coordinate Analysis - Scree Plot") +
+  xlab("Principal Component") + 
+  ylab("Percent of Explained Variance")
+
+pdf("PCoA_Scree.pdf")
+pcoa_scree
+dev.off()
+
+#PCoA with different groupings explained. 
+hostgg <- ggplot(mbov.pcoa, aes(x = PC2, y = PC3, colour = Host, label = row.names(mbov.pcoa))) + 
+  geom_point() + 
   theme_bw()
 
-plot_ly(x=mbov.pcoa$PC1, y=mbov.pcoa$PC2, z=mbov.pcoa$PC3, type="scatter3d", mode="markers", color=mbov.pcoa$Host)
+speciesgg <- ggplot(mbov.pcoa, aes(x = PC2, y = PC3, colour = Species, label = row.names(mbov.pcoa))) + 
+  geom_point() + 
+  theme_bw()
+
+countrygg <- ggplot(mbov.pcoa, aes(x = PC2, y = PC3, colour = Country, label = row.names(mbov.pcoa))) + 
+  geom_point() + 
+  theme_bw()
+
+wildlifegg <- ggplot(mbov.pcoa, aes(x = PC2, y = PC3, colour = Cattle, label = row.names(mbov.pcoa))) + 
+  geom_point() + 
+  theme_bw()
+
+pdf("PCoA_AccessoryGenome.pdf")
+hostgg
+countrygg
+speciesgg
+wildlifegg
+dev.off()
+
+mbov.pcoa_patch <- (hostgg | countrygg)/(wildlifegg | speciesgg)
+#These are 3D versions of the plots, good for further visualization but not necessary
+
+#PC's 1,2,3
+plot_ly(x=mbov.pcoa$PC1, y=mbov.pcoa$PC2, z=mbov.pcoa$PC3, type="scatter3d", mode="markers", color = mbov.pcoa$PC4)
+
+#PC's 1,3,4
+plot_ly(x=mbov.pcoa$PC1, y=mbov.pcoa$PC3, z=mbov.pcoa$PC4, type="scatter3d", mode="markers", color = mbov.pcoa$Species)
+
+# 4 Dimensions + Country of Origin 
+mbov.pcoa_5D <- ggplot(mbov.pcoa, aes(x = PC3, y = PC4, color = PC1, shape = Country, label = row.names(mbov.pcoa))) + 
+  geom_point(aes(size = PC2)) + 
+  theme_bw() +
+  ggtitle("All 4 Dimensions of PCoA")
+
+pdf("PCoA_AllDimension.pdf")
+mbov.pcoa_5D
+dev.off()
+
+pcoa_patch <- mbov.pcoa_5D | pcoa_scree
+#### 7.
